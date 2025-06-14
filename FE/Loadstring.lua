81e8bb19999c6869d78ea62c409c24f0494b6091
relojac <121554584+relojac@users.noreply.github.com>
2025-06-14 18:10:36 +0800

    Create Loadstring.lua

---
+++ FE/Loadstring.lua
@@ -0,0 +1,8 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/relojac/spawn/refs/heads/main/FE/MobileButtonsInit.lua"))()
+
+task.wait(1)
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/relojac/spawn/refs/heads/main/FE/Sit.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/relojac/spawn/refs/heads/main/FE/Sprint.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/relojac/spawn/refs/heads/main/FE/Stun.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/relojac/spawn/refs/heads/main/FE/Suicide.lua"))()
