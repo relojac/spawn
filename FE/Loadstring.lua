fbdda22ef98136ff97349c6ab76ed07fd840b3b1
relojac <121554584+relojac@users.noreply.github.com>
2025-06-15 08:03:51 +0800

    Update Loadstring.lua

--- FE/Loadstring.lua
+++ FE/Loadstring.lua
@@ -1,1 +1,26 @@
+local Global = (getgenv and getgenv()) or shared
+Global.SpawnUtilsConfig = {
+	["Sprint"] = {
+		["Easing"] = {
+			["In"] = {
+				["Time"] = 0.75,
+				["Style"] = Enum.EasingStyle.Sine,
+				["Direction"] = Enum.EasingDirection.InOut
+			},
+			["Out"] = {
+				["Time"] = 1,
+				["Style"] = Enum.EasingStyle.Exponential,
+				["Direction"] = Enum.EasingDirection.Out
+			}
+		},
+		["Multipliers"] = {
+			["WalkSpeed"] = 1.5,
+			["FieldOfView"] = 1.3
+		}
+	},
+	["Suicide"] = {
+		["Tween"] = false
+	}
+}
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/relojac/spawn/refs/heads/main/FE/Main.lua"))()
