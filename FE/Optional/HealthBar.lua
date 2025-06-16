b7c70bd37f1a8042bf75a5727332dda0f7be18a5
relojac <121554584+relojac@users.noreply.github.com>
2025-06-16 21:00:53 +0800

    Update HealthBar.lua

--- FE/Optional/HealthBar.lua
+++ FE/Optional/HealthBar.lua
@@ -12,15 +12,19 @@ Player.CharacterAdded:Connect(function(char)
 	Humanoid = Character:WaitForChild("Humanoid")
 end)
 
+for _, v in ipairs(PlayerGui:GetChildren()) do
+	if v:IsA("ScreenGui") and v.Name == "HealthGuiLocal" then v:Destroy() end
+end
+
 local HealthGui = Instance.new("ScreenGui", PlayerGui)
 	HealthGui.Name = "HealthGuiLocal"
-	HealthGui.ResetOnSpawn = true
+	HealthGui.ResetOnSpawn = false
 	HealthGui.IgnoreGuiInset = true
 
 local Frame = Instance.new("Frame", HealthGui)
 	Frame.Name = "HealthFrame"
 	Frame.Size = UDim2.new(0, 200, 0, 20)
-	Frame.Position = UDim2.new(0, 0, 1, -35)
+	Frame.Position = UDim2.new(0.5, 0, 1, -55)
 	Frame.AnchorPoint = Vector2.new(0.5, 1)
 	Frame.BackgroundColor3 = Color3.fromRGB(69, 69, 69)
 	Frame.ClipsDescendants = true
@@ -30,7 +34,6 @@ local Bar = Instance.new("Frame", Frame)
 	Bar.Size = UDim2.new(1, 0, 1, 0)
 	Bar.Position = UDim2.new(0, 0, 0, 0)
 	Bar.BackgroundColor3 = Color3.fromRGB(255, 95, 95)
-	Bar.BorderSizePixel = 0
 
 local HP = Instance.new("TextLabel", Frame)
 	HP.Name = "HP"
@@ -38,8 +41,8 @@ local HP = Instance.new("TextLabel", Frame)
 	HP.TextColor3 = Color3.new(1, 1, 1)
 	HP.BackgroundTransparency = 1
 	HP.Size = UDim2.new(0.5, 0, 1, 0)
-	HP.Position = UDim2.new(0, 5, 0, 0)
-	HP.TextSize = 10
+	HP.Position = UDim2.new(0, 10, 0, 0)
+	HP.TextSize = 13
 	HP.TextXAlignment = Enum.TextXAlignment.Left
 	HP.ZIndex = 1
 
@@ -50,7 +53,7 @@ local Corner2 = Corner:Clone()
 
 local Stroke = Instance.new("UIStroke", Frame)
 	Stroke.Color = Color3.fromRGB(69, 69, 69)
-	Stroke.Thickness = 2
+	Stroke.Thickness = 3
 local Stroke2 = Stroke:Clone()
 	Stroke2.Parent = Bar
 	
