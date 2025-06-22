72a85da3ffeff4aa333f90e27ab676e7413da1c9
relojac <121554584+relojac@users.noreply.github.com>
2025-06-22 15:51:01 +0800

    Create NightVision.lua

---
+++ FE/NightVision.lua
@@ -0,0 +1,85 @@
+local execStart = tick() -- *starts timer* okay time to write code
+
+local Players = game:GetService("Players")
+local UserInputService = game:GetService("UserInputService")
+local RunService = game:GetService("RunService")
+local Lighting = game:GetService("Lighting")
+local SoundService = game:GetService("SoundService")
+
+local Player = Players.LocalPlayer
+local PlayerGui = Player.PlayerGui
+
+local MobileButtons = PlayerGui:WaitForChild("MobileButtonsLocal")
+local JumpButtonFrame = MobileButtons:WaitForChild("JumpButtonFrame") -- This has a separate script that uses Math and the Screen's AbsoluteSize to move it to the jump button.
+
+local NVButton = Instance.new("ImageButton")
+	NVButton.Name = "NVButton"
+	NVButton.Position = UDim2.new(-3.3, 0, 0, 0) -- This is not offscreen, as its Position is relative to its parent. This should be to the top-left of the jump button.
+	NVButton.Size = UDim2.new(1, 0, 1, 0)
+	NVButton.BackgroundTransparency = 1
+	NVButton.Active = true
+  NVButton.Image = "rbxassetid://120316668670756"
+	NVButton.Parent = JumpButtonFrame
+
+local NV = Instance.new("TextLabel")
+	NV.Name = "NVText" 
+	NV.Size = UDim2.new(1, 0, 1, 0)
+	NV.Text = "NightVision"
+	NV.BackgroundTransparency = 1
+	NV.TextSize = 12
+	NV.TextColor3 = Color3.new(1, 1, 1)
+	NV.TextStrokeColor3 = Color3.new(0, 0, 0)
+	NV.TextStrokeTransparency = 0
+	NV.ZIndex = 1
+	NV.Parent = NVButton
+
+local NVE_1 = Instance.new("ColorCorrectionEffect", Lighting)
+	NVEffect.Name = "NVE_1"
+	NVEffect.Enabled = false
+	NVEffect.TintColor = Color3.fromRGB(153, 255, 208)
+	NVEffect.Contrast = 0.2
+	NVEffect.Saturation = -1
+
+local VignetteGui = Instance.new("ScreenGui", PlayerGui)
+	VignetteGui.Name = "VignetteGuiLocal"
+	VignetteGui.IgnoreGuiInset = false
+	VignetteGui.Enabled = false
+
+local Vignette = Instance.new("ImageLabel", VignetteGui)
+	Vignette.Name = "Vignette"
+	Vignette.ImageTransparenxy = 0.8
+	Vignette.Image = "" -- placeholder first
+	Vignette.Size = UDim2.new(1, 0, 1, 0)
+
+local SoundOn = Instance.new("Sound", SoundService)
+	SoundOn.SoundId = ""
+local SoundOff = Instance.new("Sound", SoundService)
+	SoundOff.SoundId = "" 
+
+local nv = false
+local function nvon()
+	if not nv then nv = true else nv = false end
+end
+
+RunService.Heartbeat:Connect(function()
+	if nv then
+		NVButton.Image = "rbxassetid://125086742998263"
+		VignetteGui.Enabled = true
+		NVE_1.Enabled = true
+	else
+		NVButton.Image = "rbxassetid://120316668670756"
+		VignetteGui.Enabled = false
+		NVE_1.Enabled = false
+	end
+end)
+
+NVButton.MouseButton1Click:Connect(nvon)
+
+UserInputService.InputBegan:Connect(function(input)
+	if input.KeyCode == Enum.KeyCode.KeypadSeven then
+		nvon()
+	end
+end)
+
+local execEnd = tick() -- *stops timer* okay done :3
+print("Loaded in " .. tostring(execEnd-execStart) .. ".") -- Basically this should print the amount of time to took t o load this
